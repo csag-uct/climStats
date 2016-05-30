57069afbdd793512ce7933369845fb1bfa9f83c7
Chris Jack <cjack@csag.uct.ac.za>
2016-05-30 15:03:53 +0200

    Added median and percentile functions, these really should be parameterized later

--- climstats/functions.py
+++ climstats/functions.py
@@ -1,6 +1,14 @@
 import numpy as np
 import scipy
 
+def make_percentile_function(percentile):
+
+	def f(data, axis):
+		data = np.ma.filled(data, np.nan)
+		return np.nanpercentile(data, percentile, axis=axis)
+
+	return f
+
 def not_masked(data, axis):
 	return data.getmaskarray().all(axis=0).astype(np.int16)
 
@@ -13,6 +21,7 @@ def generic(data, func, axis=0, above=None, below=None, **kwargs):
 		data = np.ma.masked_array(data, data >= below)
 
 	#print func(data, axis=axis)
+	#print np.ma.count(data, axis=axis)
 
 	return np.ma.filled(func(data, axis=axis), fill_value=0.0)
 
@@ -29,8 +38,23 @@ def maximum(data, **kwargs):
 def minimum(data, **kwargs):
 	return generic(data, np.ma.min, **kwargs)
 
+def median(data, **kwargs):
+	return generic(data, make_percentile_function(50), **kwargs)
+
+def percentile90th(data, **kwargs):
+	return generic(data, make_percentile_function(90), **kwargs)
+
+def percentile95th(data, **kwargs):
+	return generic(data, make_percentile_function(95), **kwargs)
+
+def percentile99th(data, **kwargs):
+	return generic(data, make_percentile_function(99), **kwargs)
+
 def days(data, axis=0, **kwargs):
 	return generic(data, np.ma.count, **kwargs)
+
+
+#def spell_days(data, func)
 
 
 def window_generic(data, func, axis=0, window=1, above=None, below=None, window_func=np.ma.sum):
@@ -74,11 +98,16 @@ def window_days(data, **kwargs):
 	return window_generic(data, np.ma.count, window_func=not_masked, **kwargs)
 
 
+
 registry = {
 	'mean': {'function': mean, 'units':None },
 	'total': {'function': total, 'units':None },
 	'maximum': {'function': maximum, 'units':None },
 	'minimum': {'function': minimum, 'units':None },
+	'median': {'function': median, 'units':None},
+	'percentile90th': {'function': percentile90th, 'units':None},
+	'percentile95th': {'function': percentile95th, 'units':None},
+	'percentile99th': {'function': percentile99th, 'units':None},
 	'days': {'function': days, 'units':'days'},
 	'rolling_maximum': {'function': window_maximum, 'units':None},
 	'rolling_days': {'function': window_days, 'units':'days'}
